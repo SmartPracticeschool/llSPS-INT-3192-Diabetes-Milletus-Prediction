2624f15bdd4d18b60478e6d0f78ed6b821fc5c03
NityaYalala <67637608+NityaYalala@users.noreply.github.com>
2020-07-06 16:09:51 +0530

    Add files via upload

---
+++ Project ML Flask/flask/app.py
@@ -0,0 +1,44 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+from joblib import load
+
+app = Flask(__name__)
+model = pickle.load(open('logistic.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('diabetes.html')
+
+@app.route('/y_predict',methods=['POST'])
+def y_predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    x_test = [[float(x) for x in request.form.values()]]
+    print(x_test)
+    sc=load('patient.save')
+    prediction = model.predict(sc.transform(x_test))
+    print(prediction)
+    output=prediction[0]
+
+    if(output==0):
+        pred=" Non-Diabetic "
+        return render_template('diabetes.html', prediction_text='Status: {}'.format(pred))
+    else:
+        pred="Diabetic"
+        return render_template('diet.html',prediction_text='Status: {}'.format(pred))
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    '''
+    For direct API calls through request
+    '''
+    data = request.get_json(force=True)
+    prediction = model.y_predict([np.array(list(data.values()))])
+
+    output = prediction[0]
+    return jsonify(output)
+
+if __name__ == "__main__":
+    app.run(debug=True)
